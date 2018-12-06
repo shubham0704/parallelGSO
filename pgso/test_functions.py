b06b369ac4ad786850a7fa4971e202ea395ccc27
Souldiv <amrut.phadke2016@vitstudent.ac.in>
2018-12-06 21:30:35 +0530

    added more unimodal n-dimensional functions

--- pgso/test_functions.py
+++ pgso/test_functions.py
@@ -7,16 +7,7 @@ from scipy.optimize import minimize
 from numpy import abs, cos, exp, mean, pi, prod, sin, sqrt, sum
 import math,pylab
 
-
-### Sphere Function
-
-def sphere( x ):
-    x = np.asarray_chkfinite(x)
-    return sum( x**2 )
-
-
 ### Rosenbrock (Multimodal) Function
-
 def rosen(x):
     """
     The Rosenbrock's function of N variables
@@ -25,28 +16,13 @@ def rosen(x):
 
     return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
 
-
 ### Rastrigin Function
-
 def rastrigin( x ):  # rast.m
     x = np.asarray_chkfinite(x)
     n = len(x)
     return 10*n + sum( x**2 - 10 * cos( 2 * pi * x ))
 
-
-### Griewangk Function
-
-def griewank( x, fr=4000 ):
-    x = np.asarray_chkfinite(x)
-    n = len(x)
-    j = np.arange( 1., n+1 )
-    s = sum( x**2 )
-    p = prod( cos( x / sqrt(j) ))
-    return s/fr - p + 1
-
-
 ### Ackley Function
-
 def ackley( x, a=20, b=0.2, c=2*pi ):
     x = np.asarray_chkfinite(x)  # ValueError if any NaN or Inf
     n = len(x)
@@ -54,9 +30,7 @@ def ackley( x, a=20, b=0.2, c=2*pi ):
     s2 = sum( cos( c * x ))
     return -a*exp( -b*sqrt( s1 / n )) - exp( s2 / n ) + a + exp(1)
 
-
 ### Weierstrass Function
-
 def wer(a, x, e): # a is givin number, e is error tolerance
 
     Sum1 = 0
@@ -90,19 +64,7 @@ def append(x0, xf, n):
     return xl, yl
 
 
-### Zakharov Function
-
-
-def zakharov( x ):  # zakh.m
-    x = np.asarray_chkfinite(x)
-    n = len(x)
-    j = np.arange( 1., n+1 )
-    s2 = sum( j * x ) / 2
-    return sum( x**2 ) + s2**2 + s2**4
-
-
-# ### Non-Continuous Rastrigin Function
-
+### Non-Continuous Rastrigin Function
 def nonContinuousRastrigin(x):
     y=[]
     for i in range(len(x)):
@@ -116,7 +78,78 @@ def nonContinuousRastrigin(x):
     n=len(x); A=10.
     return 10*n + sum( y**2 - 10 * cos( 2 * pi * y ))
 
+## Unimodal Functions
 
+### Bochevsky 2-dimensional for xi [-100, 100]
+def bohachevsky(x):
+    x = np.asarray_chkfinite(x)
+    # X[1] X[2]
+    foo = x[0]**2 + 2 * x[1]**2 - 0.3 * cos(3*pi*x[0]) - 0.4 * cos(4*pi*x[1]) + 0.7
+    return foo
+
+### Matyas 2-dimensional for x,y [-10, 10]
+
+def Matyas(x):
+    x = np.asarray_chkfinite(x)
+    foo = 0.26*(x[0]**2 + x[1]**2) - 0.48 * x[0] * x[1]
+    return foo
+
+### n-dimensional xi [-1,1]
+def exponential(x):
+    x = np.asarray_chkfinite(x)
+    foo = -exp(-0.5 * sum(x**2))
+    return foo
+
+### n-dimensional xi [-1,1]
+def powellsumfcn(x):
+    x = np.asarray_chkfinite(x)
+    n = len(x)
+
+    scores = 0
+    absX = abs(x)
+    for i in range(0,n):
+        scores = scores + (absX[i] ** (i+1))
+    return scores
+
+### n-dimensional xi [-100, 100]
+def schfewel_220(x):
+    x = np.asarray_chkfinite(x)
+    foo = sum(abs(x))
+    return foo
+
+### Sum of squares function xi [-10, 10] n-dimensional
+def sum_of_squares(x):
+    x = np.asarray_chkfinite(x)
+    foo = sum([i * (x[i] ** 2) for i in range(0,len(x))])
+    return foo
+
+### n-dimensional xi [-100, 100]
+def schwefel_222(x):
+    x = np.asarray_chkfinite(x)
+    foo = sum(abs(x)) + np.prod(abs(x))
+    return foo
+
+### Griewangk Function n-dimensional
+def griewank( x, fr=4000 ):
+    x = np.asarray_chkfinite(x)
+    n = len(x)
+    j = np.arange( 1., n+1 )
+    s = sum( x**2 )
+    p = prod( cos( x / sqrt(j) ))
+    return s/fr - p + 1
+
+### Zakharov Function
+def zakharov( x ):  # zakh.m
+    x = np.asarray_chkfinite(x)
+    n = len(x)
+    j = np.arange( 1., n+1 )
+    s2 = sum( j * x ) / 2
+    return sum( x**2 ) + s2**2 + s2**4
+
+### Sphere Function
+def sphere( x ):
+    x = np.asarray_chkfinite(x)
+    return sum( x**2 )
 
 class BenchMark():
 
